b655835c76cf501f7a5a510cc7eb68a21735c0d7
Ishaan Jain <jainishaan107@gmail.com>
2025-02-02 00:54:51 +0530

    dropdown menu added

--- src/modules/admin/admin.jsx
+++ src/modules/admin/admin.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import DDropdown from '../../atoms/DDropdown'
 
 function Admin() {
     return (
         <>
         <h1>Admin</h1>
+        <DDropdown 
+            name="category" 
+            data={["Food","Travel","Grocery","Rent","Others"]}
+            func={(selected_category)=>console.log("Category Selected: ", selected_category)}
+        />
         </>
     )
 }
 
-export default Admin
+export default Admin;
