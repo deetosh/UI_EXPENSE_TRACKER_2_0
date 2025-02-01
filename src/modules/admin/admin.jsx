d067f79f868a2af42c9fa8bab55becb7c7c29bac
deetosh <deetoshkuila18@gmail.com>
2025-02-02 01:21:39 +0530

    removed admin changes

--- src/modules/admin/admin.jsx
+++ src/modules/admin/admin.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import DDropdown from '../../atoms/DDropdown'
 
 function Admin() {
     return (
         <>
         <h1>Admin</h1>
-        <DDropdown 
-            name="category" 
-            data={["Food","Travel","Grocery","Rent","Others"]}
-            func={(selected_category)=>console.log("Category Selected: ", selected_category)}
-        />
         </>
     )
 }
