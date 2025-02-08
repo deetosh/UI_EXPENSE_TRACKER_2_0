1134982f620db1334294f33b375d5054fe395914
deetosh <deetoshkuila18@gmail.com>
2025-02-08 21:43:01 +0530

    adding access-token in local-storage-cookies-not-working

--- src/services/ApiHelper.js
+++ src/services/ApiHelper.js
@@ -4,13 +4,18 @@ import axios from "axios";
 
 export const callAPI = async (URL, method = 'POST', body = null, params = null) => {
   try {
-    const base_url = "http://localhost:8000/api/v1";
+    // const base_url = "http://localhost:8000/api/v1";
+    // const base_url = "https://trackify-h2hdbjcwcubaf2cm.centralindia-01.azurewebsites.net/api/v1";
+    const base_url = "http://20.244.93.32:8000/api/v1";
     const config = {
       url: base_url + URL,
       method: method,
       data: method !== 'GET' ? body : undefined,
       params: params || undefined,
       withCredentials: true,
+      headers : {
+        'Authorization': `${localStorage.getItem('access-token')}`
+      }
     };
 
     const response = await axios(config);
